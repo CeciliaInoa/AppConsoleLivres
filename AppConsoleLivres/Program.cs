b74cff4b363c80d6d5e3298732aaaba8f57c1c78
Cecilia Inoa <300139282@monboreal.ca>
2024-10-19 05:59:18 -0400

    Première version de l'AppConsoleLivres

---
+++ AppConsoleLivres/Program.cs
@@ -0,0 +1,10 @@
+﻿
+//Devoir 1 : Gestion des livres
+//Cecilia Inoa
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
